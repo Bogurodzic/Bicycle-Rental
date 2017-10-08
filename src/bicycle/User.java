26ea2e79e39c479fbe6248eb1c6537061511f763
Kamil Jarzab <jarzbkamil@gmail.com>
2017-10-08 11:51:41 +0200

    Added user class

---
+++ src/bicycle/User.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bicycle;
+
+/**
+ *
+ * @author bogurodzica
+ */
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+    
+    public User(int id, String name, String surname){
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+    
+}
