befb28eb348317d85ca78f68fff7d98b5c1f543c
dnlolski <daniel.olszewski00@gmail.com>
2017-10-20 11:23:29 +0200

    Fixed input bug, added user recognition

--- src/bicycle/Main.java
+++ src/bicycle/Main.java
@@ -16,39 +16,50 @@ public class Main {
      */
     public static void main(String[] args) {
         
+        int userId;
+        
         BicycleRecord BicycleRecord = new BicycleRecord();
         UserRecord UserRecord = new UserRecord();
         
         Scanner reader = new Scanner(System.in);
         System.out.println("\tWelcome!\n");
-        System.out.println("Please log in!\nUser id: ");
-        int userId = reader.nextInt();
         
-        if(UserRecord.ifExists(userId)){
-           System.out.println("Index of user in list: " + UserRecord.getIndexOfUser(userId));
-           System.out.println("Id if user: " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getId());
+        System.out.print("Please log in!\nUser id: ");
+        
+        
+        if(reader.hasNextInt()){
+            userId = reader.nextInt();
+            
+            if(UserRecord.ifExists(userId)){
+                //System.out.println("Index of user in list: " + UserRecord.getIndexOfUser(userId));
+                //System.out.println("Id of user: " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getId());
+                
+                if(UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getRole() == User.Role.ADMIN){
+                    System.out.println("\tWelcome Admin " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getName());
+                    // TODO: menu for admin
+                }
+                else{
+                    System.out.println("\tWelcome " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getName() );
+                    // TODO menu for user
+                }
+                
+            }
+            else{
+                System.out.println("\tUser doesn't exist!");
+            }
+
         }
         else{
-            System.out.println("User doesn't exist!");
+            System.out.println("\tInvalid input!");
         }
-        //for( user : allUsers)
- 
-        
-        
-        
-        
-                
-     
-        
-        
-        
+
 //        BicycleRecord.bicycle1.addUserId(userId);
 //        
 //        System.out.println(UserRecord.allUsers.get(0).getId());
 //        System.out.println(BicycleRecord.allBicycle.get(0).getUserId());
 //        System.out.println(BicycleRecord.allBicycle.get(0).getBicycleId());
 //        System.out.println(BicycleRecord.allBicycle.get(1).getBicycleId());
-//            BicycleRecord.removeBicycleFromRecord();
+//        BicycleRecord.removeBicycleFromRecord();
        
     }
     
