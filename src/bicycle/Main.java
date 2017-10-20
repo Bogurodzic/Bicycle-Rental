48b76724bfa1ad00d03a35f6b363665d6bf57df7
dnlolski <daniel.olszewski00@gmail.com>
2017-10-20 17:35:44 +0200

    Added user creation (adding to list), displaying all users

--- src/bicycle/Main.java
+++ src/bicycle/Main.java
@@ -17,44 +17,84 @@ public class Main {
     public static void main(String[] args) {
         
         int userId;
+        int id;
+        String surname = null;
+        String name = null;
+        int option;
         
         BicycleRecord BicycleRecord = new BicycleRecord();
         UserRecord UserRecord = new UserRecord();
-        
+       
+        while(true){
         Scanner reader = new Scanner(System.in);
-        System.out.println("\tWelcome!\n");
+        System.out.println("\tWELCOME!\n");
+        System.out.println("Please log in!\nOr press 0 to exit\nUser id: ");
         
-        System.out.print("Please log in!\nUser id: ");
         
-        
-        if(reader.hasNextInt()){
+        if(reader.hasNextInt()){ //consider while loop
             userId = reader.nextInt();
+            switch(userId){
+                case(0):
+                    System.out.println("Closing... \n \tThank you! ");
+                    System.exit(0);
+            }
             
             if(UserRecord.ifExists(userId)){
                 //System.out.println("Index of user in list: " + UserRecord.getIndexOfUser(userId));
                 //System.out.println("Id of user: " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getId());
-                
                 if(UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getRole() == User.Role.ADMIN){
                     System.out.println("\tWelcome Admin " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getName());
-                    // TODO: menu for admin
+                    // TODO: finish menu for admin
+                    System.out.println("Menu: \n");
+                    System.out.println("1. Add user\n2. Show users\n3. Remove user\n4. Edit user\n5. Logout");
+                        while(reader.hasNextInt()){
+                            option = reader.nextInt();
+                            switch(option){
+                                case(1): 
+                                    System.out.println("\tADD USER");
+                                    System.out.println("Enter ID: ");
+                                    id = reader.nextInt();
+                                    System.out.println("Enter name: ");
+                                    name = reader.next();
+                                    System.out.println("Enter surname: ");
+                                    surname = reader.next();
+                                    System.out.println("Pick role:\n1.Admin\n2.User");
+                                    option = reader.nextInt();
+                                        switch(option){
+                                        case(1):
+                                            User user4 = new User (id, name, surname, User.Role.ADMIN); //need to be changed, name of object need to be dynamic
+                                            UserRecord.allUsers.add(user4);
+                                            break;
+                                        case(2):
+                                            User user5 = new User (id, name, surname, User.Role.USER);
+                                            UserRecord.allUsers.add(user5);
+                                            break;
+                                        }
+                                    System.out.println("1. Add user\n2. Show users\n3. Remove user\n4. Edit user\n5. Logout");
+                                    break;
+                                    
+                                case(2):
+                                    UserRecord.getAllUsers();
+                                    System.out.println("1. Add user\n2. Show users\n3. Remove user\n4. Edit user\n5. Logout");
+                                    break;
+                            }
+                        }
                 }
                 else{
                     System.out.println("\tWelcome " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getName() );
-                    // TODO menu for user
-                }
-                
+                    // TODO: menu for user
+                } 
             }
             else{
-                System.out.println("\tUser doesn't exist!");
+                System.out.println("\tUser doesn't exist! Try again \n");
             }
-
         }
         else{
-            System.out.println("\tInvalid input!");
+            System.out.println("\tInvalid input! Try again \n");
+        }
         }
 
 //        BicycleRecord.bicycle1.addUserId(userId);
-//        
 //        System.out.println(UserRecord.allUsers.get(0).getId());
 //        System.out.println(BicycleRecord.allBicycle.get(0).getUserId());
 //        System.out.println(BicycleRecord.allBicycle.get(0).getBicycleId());
