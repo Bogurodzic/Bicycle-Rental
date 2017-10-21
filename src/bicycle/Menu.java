a5687c9b15944d88e7b8e1a150320587dc95ad9b
Kamil Jarzab <jarzbkamil@gmail.com>
2017-10-21 15:14:16 +0200

    Code refactorisation

--- src/bicycle/Menu.java
+++ src/bicycle/Menu.java
@@ -23,71 +23,113 @@ public class Menu {
     
     public void start(){
         while(true){
-            Scanner reader = new Scanner(System.in);
-            System.out.println("\tWELCOME!\n");
-            System.out.println("Please log in!\nOr press 0 to exit\nUser id: ");
-
+        Scanner reader = initMessage();
 
             if(reader.hasNextInt()){ //consider while loop
                 userId = reader.nextInt();
+                
                 switch(userId){
                     case(0):
-                        System.out.println("Closing... \n \tThank you! ");
-                        System.exit(0);
+                        exitMessage();
                 }
 
                 if(UserRecord.ifExists(userId)){
                     if(UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getRole() == User.Role.ADMIN){
-                        System.out.println("\tWelcome Admin " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getName());
-                        // TODO: finish menu for admin
-                        System.out.println("Menu: \n");
-                        System.out.println("1. Add user\n2. Show users\n3. Remove user\n4. Edit user\n5. Logout");
+                        infoMessage();
+                        menuMessage();
                             while(reader.hasNextInt()){
                                 option = reader.nextInt();
                                 switch(option){
                                     case(1): 
                                         System.out.println("\tADD USER");
-                                        System.out.println("Enter ID: ");
-                                        id = reader.nextInt();
-                                        System.out.println("Enter name: ");
-                                        name = reader.next();
-                                        System.out.println("Enter surname: ");
-                                        surname = reader.next();
-                                        System.out.println("Pick role:\n1.Admin\n2.User");
-                                        option = reader.nextInt();
+                                        id = enterId(reader);
+                                        name = enterName(reader);
+                                        surname = enterSurname(reader);
+                                        option = enterRole(reader);
                                             switch(option){
                                             case(1):
-                                                User user4 = new User (id, name, surname, User.Role.ADMIN); //need to be changed, name of object need to be dynamic
-                                                UserRecord.allUsers.add(user4);
+                                                UserRecord.allUsers.add(new User (id, name, surname, User.Role.ADMIN));
                                                 break;
                                             case(2):
-                                                User user5 = new User (id, name, surname, User.Role.USER);
-                                                UserRecord.allUsers.add(user5);
+                                                UserRecord.allUsers.add(new User (id, name, surname, User.Role.USER));
                                                 break;
                                             }
-                                        System.out.println("1. Add user\n2. Show users\n3. Remove user\n4. Edit user\n5. Logout");
+                                        menuMessage();
                                         break;
 
                                     case(2):
                                         UserRecord.getAllUsers();
-                                        System.out.println("1. Add user\n2. Show users\n3. Remove user\n4. Edit user\n5. Logout");
+                                        menuMessage();
                                         break;
                                 }
                             }
                     }
                     else{
-                        System.out.println("\tWelcome " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getName() );
+                        welcomeMessage();
                         // TODO: menu for user
                     } 
                 }
                 else{
-                    System.out.println("\tUser doesn't exist! Try again \n");
+                    userDoesNotExistMessage();
                 }
             }
             else{
-                System.out.println("\tInvalid input! Try again \n");
+                invalidInputMessage();
             }
         }
+    }
+    
+    
+    private Scanner initMessage(){
+        System.out.println("\tWELCOME!\n");
+        System.out.println("Please log in!\nOr press 0 to exit\nUser id: ");
+        return new Scanner(System.in);
+    }
+    
+    private void exitMessage(){
+        System.out.println("Closing... \n \tThank you! ");
+        System.exit(0);
+    }
+    
+    private void infoMessage(){
+        System.out.println("\tWelcome Admin " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getName());
+        System.out.println("Menu: \n");
+    }
+    
+    private void menuMessage(){
+        System.out.println("1. Add user\n2. Show users\n3. Remove user\n4. Edit user\n5. Logout");
+    }
+    
+    private void welcomeMessage(){
+        System.out.println("\tWelcome " + UserRecord.allUsers.get(UserRecord.getIndexOfUser(userId)).getName());
+    }
+    
+    private void invalidInputMessage(){
+        System.out.println("\tInvalid input! Try again \n");
+    }
+    
+    private void userDoesNotExistMessage(){
+        System.out.println("\tUser doesn't exist! Try again \n");
+    }
+    
+    private int enterId(Scanner reader){
+        System.out.println("Enter ID: ");
+        return reader.nextInt();
+    }
+    
+    private String enterName(Scanner reader){
+        System.out.println("Enter name: ");
+        return reader.next();
+    }
+    
+    private String enterSurname(Scanner reader){
+        System.out.println("Enter surname: ");
+        return reader.next();
+    }
+    
+    private int enterRole(Scanner reader){
+        System.out.println("Pick role:\n1.Admin\n2.User");
+        return reader.nextInt();
     }
     
 }
