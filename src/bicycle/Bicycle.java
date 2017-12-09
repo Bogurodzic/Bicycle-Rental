4ba57ed958e15cdcd9f70bd5b8dd4d5901de61d9
Kamil Jarzab <jarzbkamil@gmail.com>
2017-12-10 00:50:27 +0100

    Added basic timing feature. Calculate time between rent and return

--- src/bicycle/Bicycle.java
+++ src/bicycle/Bicycle.java
@@ -20,9 +20,11 @@ enum State {
 
 public class Bicycle {
     private int bicycleId;
+    private int timeLimit = 5;
     private String Station;
     private Date rentTime;
     private Date returnTime;
+    private long rentDuration;
     
     private State status = State.AVAILABLE;
     //private Location location;
@@ -53,13 +55,13 @@ public class Bicycle {
     private void rentTime(){
         this.rentTime = new Date();
         //System.out.println(this.rentTime.toString());
-        System.out.println("You have rented bicycle");
+//        System.out.println("You have rented bicycle");
     }
     
     private void returnTime(){
         this.returnTime = new Date();
         //System.out.println(this.returnTime.toString());
-        System.out.println("You have rented bicycle");
+//        System.out.println("You have returned bicycle");
     }
     
     public void handleRentRequest(){
@@ -70,7 +72,27 @@ public class Bicycle {
     public void handleReturnRequest(){
         this.makeAvaliable();
         this.returnTime();
+        this.calculatTimeDifference(this.rentTime, this.returnTime);
     }
+    
+    public void calculatTimeDifference(Date time1, Date time2){
+        this.rentDuration = (time2.getTime() - time1.getTime())/1000;      
+    }
+    
+    public boolean isRentTimeExceed(){
+        return this.rentDuration>this.timeLimit;
+    }
+    
+    public String handleRentTime(){
+        calculatTimeDifference(this.rentTime, this.returnTime);
+        if(this.isRentTimeExceed()){
+            return "Przekroczono czas usługi";
+        } else {
+            return "Zostało jeszcze" + (this.timeLimit - this.rentDuration);
+        }
+    };
+    
+    
     
 
     
--- src/bicycle/User.java
+++ src/bicycle/User.java
@@ -112,7 +112,7 @@ public class User {
             System.out.println("Bicycle " 
                                 + BicycleRecord.allBicycle.get(i).getBicycleId()+" "
                                 + BicycleRecord.allBicycle.get(i).getStation()+" "
-                                + BicycleRecord.allBicycle.get(i).getState());
+                                + BicycleRecord.allBicycle.get(i).getState()+" ");
         } 
     }
     
@@ -157,6 +157,20 @@ public class User {
             }
             
         }
+    }
+    
+    public String checkTime(int bicycleId){
+        String info = "Żaden rower nie jest wypożyczony";
+        
+        for (int i = 0; i < BicycleRecord.allBicycle.size(); i++){
+            if(bicycleId == BicycleRecord.allBicycle.get(i).getBicycleId()){
+                info = BicycleRecord.allBicycle.get(i).handleRentTime();
+            }
+        }
+        System.out.println("odpala sie");
+        System.out.println(info);
+        return info;
+        
     }
     
     
